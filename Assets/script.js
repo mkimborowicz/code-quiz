83a386a7ee3b8c22f14e4a3f61466ef5c5cab1e5
maddy <maddykimborowicz@gmail.com>
2021-11-28 11:13:58 -0800

    added message at end of game

--- Assets/script.js
+++ Assets/script.js
@@ -5,10 +5,12 @@ var quizContent = document.querySelector(".quiz-content");
 var questionTitle = document.querySelector("#questionTitle");
 var quizChoices = document.querySelector("#quizChoices");
 var gameOver = document.querySelector(".end-game");
+var endMessage = document.querySelector("#end-message");
 var index = 0;
 var secondsLeft = 60;
 var timeInterval;
 
+// lists questions, answers, and correct answer
 var questions = [{
 
     question: "Commonly used data types do NOT include _____",
@@ -29,6 +31,11 @@ var questions = [{
     question: "String values must be enclosed within _____ when being assigned to variables",
     choices: ["commas", "curly brackets", "quotes", "parenthesis"],
     correctAnswer: "quotes"
+},
+{
+    question: "A very useful tool during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
+    correctAnswer: "console log"
 }];
 
 
@@ -58,35 +65,15 @@ function countdown() {
         }
 
         else {
-            timer.textContent = "time up!";
+            endGame();
         }
     }
         , 1000);
 }
 
-
-
-function nextQuestion() {
-    questionTitle.textContent = questions[index].question
-
-    quizChoices.innerHTML = '';
-
-    for (let i = 0; i < questions[index].choices.length; i++) {
-        var choiceBtn = document.createElement('button');
-        choiceBtn.setAttribute('class', 'choice-button')
-        choiceBtn.setAttribute('value', questions[index].choices[i])
-
-        choiceBtn.textContent = questions[index].choices[i];
-
-        choiceBtn.onclick = answerQuestion;
-
-        quizChoices.append(choiceBtn)
-
-
-    }
-}
-
+// function when user clicks an answer, determines if it is correct or not
 function answerQuestion() {
+    // docks 5 seconds for incorrect answer
     console.log(this.value)
     if (this.value !== questions[index].correctAnswer) {
         secondsLeft -= 5
@@ -103,12 +90,30 @@ function answerQuestion() {
 
 }
 
+function nextQuestion() {
+    questionTitle.textContent = questions[index].question
+
+    quizChoices.innerHTML = '';
+
+    for (let i = 0; i < questions[index].choices.length; i++) {
+        var choiceBtn = document.createElement('button');
+        choiceBtn.setAttribute('class', 'choice-button')
+        choiceBtn.setAttribute('value', questions[index].choices[i])
+
+        choiceBtn.textContent = questions[index].choices[i];
+
+        choiceBtn.onclick = answerQuestion;
+
+        quizChoices.append(choiceBtn)
+    }
+}
 
 function endGame() {
     clearInterval(timeInterval)
     quizContent.innerHTML = '';
     gameOver.setAttribute("style", "display: block");
     timer.setAttribute("style", "display:none");
+    endMessage.textContent = "All done! Your final score is " + secondsLeft + "!";
 }
 
 startButton.addEventListener("click", startGame);
